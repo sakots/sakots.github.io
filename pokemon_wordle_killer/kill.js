ee47074b551291948d369d4a88086aeb95c3f653
sakots <sakots8000@gmail.com>
2022-03-29 01:52:49 +0900

    v2

--- pokemon_wordle_killer/kill.js
+++ pokemon_wordle_killer/kill.js
@@ -1,11 +1,14 @@
 function kill() {
-    let kill_class = document.pokemon_killer.char.value;
+    const kill_class = document.pokemon.char.value;
     console.log (kill_class);
-    //kill_classname = "'" + kill_class + "'";
-    //console.log (kill_classname);
-    let target = document.getElementsByClassName(kill_class);
-    console.log (target);
-    for (let i = 0; i < target.length; i++) {
-        target[i].style.display = "none";
+    let kill_class_split = kill_class.split('');
+    console.log (kill_class_split);
+    for (let i =0; i < kill_class_split.length; i++) {
+        let target_class = kill_class_split[i]
+        let target_char = document.getElementsByClassName(target_class);
+        console.log (target_char);
+        for (let j = 0; j < target_char.length; j++) {
+            target_char[j].style.display = "none";
+        }
     }
 }
