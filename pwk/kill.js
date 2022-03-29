11cd73cfe27f2648a5a2aa290e542cb013f2fea6
sakots <sakots8000@gmail.com>
2022-03-30 05:46:27 +0900

    ;

--- pwk/kill.js
+++ pwk/kill.js
@@ -4,7 +4,7 @@ function kill() {
     let kill_class_split = kill_class.split('');
     console.log (kill_class_split);
     for (let i =0; i < kill_class_split.length; i++) {
-        let target_class = kill_class_split[i]
+        let target_class = kill_class_split[i];
         let target_char = document.getElementsByClassName(target_class);
         console.log (target_char);
         for (let j = 0; j < target_char.length; j++) {
