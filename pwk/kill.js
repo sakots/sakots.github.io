004c6212c7e3a54a80898bf571400537f6a698ce
sakots <sakots8000@gmail.com>
2026-02-25 12:31:13 +0900

    pwk

--- pwk/kill.js
+++ pwk/kill.js
@@ -1,14 +1,50 @@
 function kill() {
-    const kill_class = document.pokemon.char.value;
-    console.log (kill_class);
-    let kill_class_split = kill_class.split('');
-    console.log (kill_class_split);
-    for (let i =0; i < kill_class_split.length; i++) {
-        let target_class = kill_class_split[i];
-        let target_char = document.getElementsByClassName(target_class);
-        console.log (target_char);
-        for (let j = 0; j < target_char.length; j++) {
-            target_char[j].style.display = "none";
-        }
+
+  // 入力した文字を取得
+  const kill_class = document.pokemon.char.value;
+  console.log (kill_class);
+  let kill_class_split = kill_class.split('');
+  console.log (kill_class_split);
+
+  // 入力した文字のクラスを持つ要素を消す/表示する
+  for (let i =0; i < kill_class_split.length; i++) {
+    let target_class = kill_class_split[i];
+    let target_char = document.getElementsByClassName(target_class);
+    console.log (target_char);
+    for (let j = 0; j < target_char.length; j++) {
+      const currentDisplay = window.getComputedStyle(target_char[j]).display;
+      if (currentDisplay === "none") {
+        // すでに消えている場合は表示をもとに戻す
+        target_char[j].style.display = "";
+      } else {
+        // 表示されている場合は消す
+        target_char[j].style.display = "none";
+      }
     }
+  }
+}
+function highlight() {
+
+  // 入力した文字を取得
+  const highlight_class = document.pokemon.char2.value;
+  console.log (highlight_class);
+  let highlight_class_split = highlight_class.split('');
+  console.log (highlight_class_split);
+
+  // 入力した文字のクラスを持つ要素をハイライト/戻す
+  for (let i =0; i < highlight_class_split.length; i++) {
+    let target_class = highlight_class_split[i];
+    let target_char = document.getElementsByClassName(target_class);
+    console.log (target_char);
+    for (let j = 0; j < target_char.length; j++) {
+      const currentBgColor = window.getComputedStyle(target_char[j]).backgroundColor;
+      if (currentBgColor === "rgb(255, 255, 0)") {
+        // すでにハイライトされている場合は戻す
+        target_char[j].style.backgroundColor = "";
+      } else {
+        // ハイライトされていない場合はハイライトする
+        target_char[j].style.backgroundColor = "yellow";
+      }
+    }
+  }
 }
