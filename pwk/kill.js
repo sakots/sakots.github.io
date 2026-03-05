42b64bdeea0aba8b96468e0654bc16bb3fde522e
sakots <sakots8000@gmail.com>
2026-03-06 08:59:14 +0900

    pwk

--- pwk/kill.js
+++ pwk/kill.js
@@ -6,20 +6,13 @@ function kill() {
   let kill_class_split = kill_class.split('');
   console.log (kill_class_split);
 
-  // 入力した文字のクラスを持つ要素を消す/表示する
+  // 入力した文字のクラスを持つ要素を消す
   for (let i =0; i < kill_class_split.length; i++) {
     let target_class = kill_class_split[i];
     let target_char = document.getElementsByClassName(target_class);
     console.log (target_char);
     for (let j = 0; j < target_char.length; j++) {
-      const currentDisplay = window.getComputedStyle(target_char[j]).display;
-      if (currentDisplay === "none") {
-        // すでに消えている場合は表示をもとに戻す
-        target_char[j].style.display = "";
-      } else {
-        // 表示されている場合は消す
-        target_char[j].style.display = "none";
-      }
+      target_char[j].style.visibility = "hidden";
     }
   }
 }
@@ -31,20 +24,13 @@ function highlight() {
   let highlight_class_split = highlight_class.split('');
   console.log (highlight_class_split);
 
-  // 入力した文字のクラスを持つ要素をハイライト/戻す
+  // 入力した文字のクラスを持つ要素をハイライト
   for (let i =0; i < highlight_class_split.length; i++) {
     let target_class = highlight_class_split[i];
     let target_char = document.getElementsByClassName(target_class);
     console.log (target_char);
     for (let j = 0; j < target_char.length; j++) {
-      const currentBgColor = window.getComputedStyle(target_char[j]).backgroundColor;
-      if (currentBgColor === "rgb(255, 255, 0)") {
-        // すでにハイライトされている場合は戻す
-        target_char[j].style.backgroundColor = "";
-      } else {
-        // ハイライトされていない場合はハイライトする
-        target_char[j].style.backgroundColor = "yellow";
-      }
+      target_char[j].style.backgroundColor = "yellow";
     }
   }
 }
